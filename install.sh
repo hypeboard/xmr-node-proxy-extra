fed8d405371eeec259bfaf06b4a53fa49702b07e
Newbie <linhdqtg@gmail.com>
2018-01-06 08:01:17 +0700

    Update install.sh
    Github name

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
-git clone https://github.com/sugoiporu/xmr-node-proxy
+git clone https://github.com/bobbieltd/xmr-node-proxy
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v6.9.2
