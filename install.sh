52a33b681117df6fd7747f1aef8413081ffae4d1
Newbie <linhdqtg@gmail.com>
2018-01-16 05:35:42 +0700

    Remove to avoid conflict with old proxy
    Fresh install

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
+sudo rm -rf ~/xmr-node-proxy
 git clone https://github.com/bobbieltd/xmr-node-proxy
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
