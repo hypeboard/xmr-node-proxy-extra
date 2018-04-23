77a97c62e37f8b83309595ce8cd63a9e68db8dd0
Learner <linhdqtg@gmail.com>
2018-04-24 01:06:31 +0700

    Fix undefined identifier and add 30s refresh

--- proxy.js
+++ proxy.js
@@ -1024,6 +1024,10 @@ function activateHTTP() {
 				for (let minerID in activeWorkers[workerID]){
                 			if (!activeWorkers[workerID].hasOwnProperty(minerID)) continue;
 					let miner = activeWorkers[workerID][minerID];
+					if (!miner) continue;
+					if (!miner.identifier) {
+						miner.identifier = "x";
+					}					
 					let name = (miner.identifier && miner.identifier != "x") ? miner.identifier + " (" + miner.ip + ")" : miner.ip;
 					++ totalWorkers;
 					totalHashrate += miner.avgSpeed;
@@ -1046,6 +1050,7 @@ function activateHTTP() {
 <html lang="en"><head>
 	<title>XNP Hashrate Monitor</title>
 	<meta charset="utf-8">
+	<meta http-equiv="refresh" content="15">
 	<style>
 	  html, body {
 	    font-family: 'Saira Semi Condensed', sans-serif;
