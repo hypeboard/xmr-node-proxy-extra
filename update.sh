535bc508db4ef6b094e1f0ec468616394a9fc22a
Learner <linhdqtg@gmail.com>
2018-05-12 04:09:01 +0700

    Remove nonsense update bash

--- update.sh
+++
@@ -1,6 +0,0 @@
-cd ~/xmr-node-proxy
-git checkout .
-git pull
-npm install
-echo "Proxy updated!  Please go ahead and restart with the correct pm2 command"
-echo "This is usually pm2 restart proxy, however, you can use pm2 list to check for your exact proxy command"
