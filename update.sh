f87aab3c448275f999209c8199466d0e4fc80007
Learner <linhdqtg@gmail.com>
2018-06-19 23:03:28 +0700

    Add update bash
    To update proxy

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Updating the XNP to latest version. Local commits will be stashed. Your config.json will be kept."
+cd ~/xmr-node-proxy
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git stash
+git checkout . &&\
+git pull &&\
+npm install &&\
+echo "Proxy update finished ! You still need to restart the proxy by pm2."
+echo "Recommended command : pm2 restart proxy. However, you can use pm2 list to check."
